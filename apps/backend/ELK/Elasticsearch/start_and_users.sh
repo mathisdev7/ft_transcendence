f79a3a302288eb474e4f93bbf1444c7157c5ff44
Spike-hd <hadrien.duflos@outlook.fr>
2025-07-03 02:15:52 +0200

    es testing issue

--- apps/backend/ELK/Elasticsearch/start_and_users.sh
+++ apps/backend/ELK/Elasticsearch/start_and_users.sh
@@ -9,17 +9,30 @@ until curl -k -u elastic:$ELASTIC_PASSWORD https://localhost:9200; do
   sleep 5
 done
 
+echo $ELASTIC_PASSWORD
+echo $ELASTIC_PASSWORD
+echo $ELASTIC_PASSWORD
+echo $ELASTIC_PASSWORD
+# Create users
+bin/elasticsearch-users useradd kibana_system -p "$KIBANA_PASSWORD" -r kibana_system
+bin/elasticsearch-users useradd logstash_system -p "$LOGSTASH_PASSWORD" -r logstash_system
 
-bin/elasticsearch-reset-password -u elastic
+# Attendre le processus principal Elasticsearch
+wait
 
 
-# Reset passwords via REST API
-curl -k -X PUT "https://localhost:9200/_security/user/kibana_system/_password" \
-     -H "Content-Type: application/json" \
-     -u elastic:$ELASTIC_PASSWORD \
-     -d '{"password": "'"$KIBANA_PASSWORD"'"}'
 
-curl -k -X PUT "https://localhost:9200/_security/user/logstash_system/_password" \
-     -H "Content-Type: application/json" \
-     -u elastic:$ELASTIC_PASSWORD \
-     -d '{"password": "'"$LOGSTASH_PASSWORD"'"}'
+
+# bin/elasticsearch-reset-password -u elastic
+
+
+# # Reset passwords via REST API
+# curl -k -X PUT "https://localhost:9200/_security/user/kibana_system/_password" \
+#      -H "Content-Type: application/json" \
+#      -u elastic:$ELASTIC_PASSWORD \
+#      -d '{"password": "'"$KIBANA_PASSWORD"'"}'
+
+# curl -k -X PUT "https://localhost:9200/_security/user/logstash_system/_password" \
+#      -H "Content-Type: application/json" \
+#      -u elastic:$ELASTIC_PASSWORD \
+#      -d '{"password": "'"$LOGSTASH_PASSWORD"'"}'
